ca57f2cda2897bbe80479ff166f6b87083799bae
RyanX <looe_xiao@hotmail.com>
2026-05-19 17:57:39 +0800

    refactor(plugin): add plugin metadata and lifecycle methods
    add standardized plugin name, version and description fields, implement required plugin metadata getter, add empty on_start and on_stop lifecycle hooks, and align package-level plugin info with class-level fields

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -2,3 +2,5 @@ from .plugin import ProxyReplyPlugin
 
 __all__ = ["ProxyReplyPlugin"]
 __version__ = "0.1.0"
+__plugin_name__ = "wechat-reply-delegation"
+__description__ = "Automatically reply to WeChat group messages on behalf of users"
--- plugin/plugin.py
+++ plugin/plugin.py
@@ -3,6 +3,10 @@ from .handler import ProxyReplyHandler
 
 
 class ProxyReplyPlugin:
+    name = "wechat-reply-delegation"
+    version = "0.1.0"
+    description = "Automatically reply to WeChat group messages on behalf of users"
+    
     def __init__(self, gateway):
         self.gateway = gateway
         self.handler = ProxyReplyHandler()
@@ -17,3 +21,22 @@ class ProxyReplyPlugin:
         handled = await self.handler.handle_group_message(event, self.gateway.adapters["weixin"])
         if handled:
             event.stop_propagation()
+
+    async def on_start(self):
+        pass
+
+    async def on_stop(self):
+        pass
+
+    def get_config_schema(self):
+        return {}
+
+    def get_metadata(self):
+        return {
+            "name": self.name,
+            "version": self.version,
+            "description": self.description,
+            "author": "Hermes Agent",
+            "platforms": ["weixin"],
+            "capabilities": ["group_message_handling", "auto_reply"]
+        }
